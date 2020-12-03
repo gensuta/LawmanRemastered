8b0306753830847ae489e93148b40c411aecc4a8
Gen <geneva@glowup.games>
2020-12-03 12:37:10 -0500

    Rough prototype has begun
    Imported yarnspinner plugin
    Tested movement, distance, gun handling and which way the player's facing

---
+++ Lawman_Remastered/Assets/PlayerBehavior.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    [Header("Floats")]
+    [SerializeField]
+    float speed, distanceFromEnemy;
+
+    [Space]
+
+    [Header("Booleans")]
+    [SerializeField]
+    bool isFacingForward, isHoldingGun;
+
+    [Space]
+    [SerializeField]
+    GameObject enemy;
+
+
+    SpriteRenderer sr;
+    ScenarioHandler scenario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        scenario = FindObjectOfType<ScenarioHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceFromEnemy = Vector3.Distance(transform.position,enemy.transform.position);
+
+        var dist = new Yarn.Value(Mathf.RoundToInt(distanceFromEnemy));
+
+        scenario.SetValue("$distance", dist);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isHoldingGun = true;
+            var val = new Yarn.Value(true);
+            scenario.SetValue("$isHoldingGun", val);
+
+            /*   example of yarn variable mess that worked
+             *   Debug.Log(yarnVar.GetValue("$didCry").AsBool);
+                     if (Input.GetKeyDown(KeyCode.Space))
+                         yarnVar.SetValue("$didCry", valueToSet);*/
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Move(Vector2.left);
+
+            isFacingForward = false;
+            sr.flipX = false;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Move(Vector2.right);
+
+
+            isFacingForward = true;
+            sr.flipX = true;
+        }
+    }
+
+    void Move(Vector2 dir)
+    {
+        transform.position += new Vector3(dir.x * speed, dir.y * speed, 0);
+    }
+}
---
+++ Lawman_Remastered/Assets/ScenarioHandler.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class ScenarioHandler : MonoBehaviour
+{
+    [SerializeField]
+    string[] startingNodes;// the list of nodes we can start at
+    int rand;
+
+
+    bool nodesLoaded; // since dr doesn't load nodes on awake we need this =_=
+    DialogueRunner dr;
+    InMemoryVariableStorage yarnVars;
+
+    [SerializeField]
+    float timeTillContinue, maxTime;
+
+    public bool isOptionsVisible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yarnVars = GetComponent<InMemoryVariableStorage>();
+
+        rand = Random.Range(0, startingNodes.Length);
+        dr = FindObjectOfType<DialogueRunner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isOptionsVisible) timeTillContinue -= Time.deltaTime;
+
+        if(dr.NodeExists(startingNodes[rand]) && !nodesLoaded)
+        {
+            dr.startNode = startingNodes[rand];
+            dr.StartDialogue();
+            nodesLoaded = true;
+        }
+        
+
+        if(timeTillContinue <= 0 && !isOptionsVisible)
+        {
+            Continue();
+        }
+    }
+
+    void Continue()
+    {
+        dr.Dialogue.Continue();
+        ResetTimer();
+    }
+
+    public void SetOptionsVisible(bool b)
+    {
+        isOptionsVisible = b;
+    }
+    public void ResetTimer()
+    {
+        timeTillContinue = maxTime;
+    }
+
+
+    public void SetValue(string variableName, Yarn.Value value)
+    {
+        yarnVars.SetValue(variableName, value);
+    }
+
+    // Return a value, given a variable name
+    public Yarn.Value GetValue(string variableName)
+    {
+        return yarnVars.GetValue(variableName);
+      
+    }
+
+}
